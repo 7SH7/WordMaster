d8ae9cc79cde4fa53e52d56651f82750ceae0670
7SH7 <kimseunghwan7777@gmail.com>
2023-09-04 03:44:10 +0900

    add create and read function

---
+++ src/main/java/com/mycom/word/Main.java
@@ -0,0 +1,21 @@
+package com.mycom.word;
+
+
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+  public static void main(String[] args) {
+
+    // TODO Auto-generated method stub
+    new WordManager().start();
+
+
+
+  }
+}
+
+// ctrl + shift + a -> share project 검색
+// ctrl + shift + f10 -> run the code
+
